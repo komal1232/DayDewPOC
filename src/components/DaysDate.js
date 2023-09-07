1daeff9bf064b2998bb8f88d77f83e7d6e63e06a
komal bhalerao <komalbhalerao1232@gmail.com>
2023-09-08 02:03:30 +0530

    day date

--- src/components/DaysDate.js
+++ src/components/DaysDate.js
@@ -1,68 +1,123 @@
 import {pink, yellow} from '@mui/material/colors';
-import React from 'react';
-import {Dimensions, StyleSheet, Text, TextInput, View} from 'react-native';
-
+import React, { useState } from 'react';
+import {
+  Dimensions,
+  Keyboard,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 const screenWidth = Dimensions.get('window').width;
 
+// const  DaysOptions = [
+//   "mon",
+//   "tue",
+// "wed",
+//  "thu",
+//  "fri",
+//  "sat",
+//   "sun"
+// ];
+
+
 const DaysDate = () => {
+  const [day, setDay] = useState(0);
+
+  const [dateDay,setDateDay] = useState(23);
+  const [dateMonth,setDateMonth] = useState(1);
+  const [dateYear,setDateYear] = useState(23);
+  const closeKeybord = () => {
+    Keyboard.dismiss();
+  };
+  const  DaysOptions = [
+    {id:1,lable: "mon"},
+    {id:2,lable: "tue"},
+    {id:3,lable: "wed"},
+    {id:4,lable: "thu"},
+    {id:5,lable: "fri"},
+    {id:6,lable: "sat"},
+    {id:7,lable: "sun"}
+  ];
+
+  const newArray = Object.values(DaysOptions).map(({ lable }) => lable);
+  // const  DaysOptions = [
+  //    "mon",
+  //    "tue",
+  //  "wed",
+  //   "thu",
+  //   "fri",
+  //   "sat",
+  //    "sun"
+  // ];
+
+  const function123 = (prop) =>{
+   setDay(prop)
+  }
+ 
+
   return (
-    <View style={styles.backgound}>
-    <View style={styles.container}>
-      <View style={styles.diCon}>
-        <Text> mon</Text>
-      </View>
-      <View style={styles.diCon}>
-      
-        <Text> tue</Text>
-      </View>
-      <View style={styles.diCon}>
-        <Text> wed</Text>
-      </View>
-      <View style={styles.diCon}>
-        <Text>thu</Text>
-      </View>
-      <View style={styles.diCon}>
-      
-        <Text> fri</Text>
-      </View>
-      <View style={styles.diCon}>
-        <Text>sat</Text>
-      </View>
-      <View style={styles.diCon}>
-        <Text>sun</Text>
-      </View>
-      <TextInput 
-        style={styles.dateinput}
-        placeholder="day"
-        onChangeText={console.log("komal")}
-        //value={"23"}
-        maxLength={2}
-        keyboardType='numeric'
-      />
-      <View style={styles.date}>
-        <Text> { "/"}</Text>
-      </View>
-      <TextInput 
-        style={styles.dateinput}
-        placeholder="month"
-        onChangeText={console.log("komal")}
-        //value={"23"}
-        maxLength={2}
-        keyboardType='numeric'
-      />
-      <View style={styles.date}>
-        <Text>{ "/"}</Text>
-      </View>
-      <TextInput 
-        style={styles.dateinput}
-        placeholder="year"
-        onChangeText={console.log("komal")}
-        //67value={"23"}
-        maxLength={2}
-        keyboardType='numeric'
-      />
-    </View>
-    </View>
+    <>
+      <TouchableWithoutFeedback onPress={closeKeybord} style={{backgroundColor:'yellow', display: 'flex'} }>
+        <>
+        <TouchableOpacity onPress={closeKeybord} style={{marginTop: 20,flex:20}}>
+        <View style={styles.backgound}>
+          <View style={styles.container}>
+            {DaysOptions.map((item) =>
+              <TouchableOpacity
+              style={{...styles.diCon , backgroundColor: item.id===day ? '#ff9966'  : '#ffcc99'}}
+              onPress={
+                ()=>{function123(item.id);}
+              }
+              key={item.id}
+              >
+              <View >
+              <Text> {item.lable}</Text>
+             </View>
+             </TouchableOpacity>
+
+            )}
+            {/* {
+              newArray.map(({lablecode})=><Text>{"hii"}</Text>)
+            } */}
+            <TextInput
+              style={styles.dateinput}
+              placeholder="day"
+              onChangeText={console.log('komal')}
+              //value={"23"}
+              maxLength={2}
+              keyboardType="numeric"
+            />
+            <View style={styles.date}>
+              <Text> {'/'}</Text>
+            </View>
+            <TextInput
+              style={styles.dateinput}
+              placeholder="month"
+              onChangeText={console.log('komal')}
+              //value={"23"}
+              maxLength={2}
+              keyboardType="numeric"
+            />
+            <View style={styles.date}>
+              <Text>{'/'}</Text>
+            </View>
+            <TextInput
+              style={styles.dateinput}
+              placeholder="year"
+              onChangeText={console.log('komal')}
+              //67value={"23"}
+              maxLength={2}
+              keyboardType="numeric"
+            />
+          </View>
+        </View>
+        </TouchableOpacity>
+        </>
+      </TouchableWithoutFeedback>
+    </>
   );
 };
 
@@ -70,40 +125,31 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'row',
-    marginTop:34,
-    marginHorizontal:4,
-
+    marginTop: 34,
+    marginHorizontal: 4,
   },
   diCon: {
     backgroundColor: '#F6E1DB',
-    justifyContent:'center',
-    alignItems:'center',
-    flex:1,
-    borderRadius:screenWidth/20,
-    height:screenWidth/(10),
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    borderRadius: screenWidth / 20,
+    height: screenWidth / 10,
 
-    margin:2
-    //padding:10,
-    //marginHorizontal:4
-
+    margin: 2,
   },
-  date:{
-    //flex:1,
-    //alignItems:'flex-end',
-    //backgroundColor: 'pink',
-    //margin:2
-    //alignItems:'center',
-    justifyContent:'center'
-
+  date: {
+    justifyContent: 'center',
   },
-  dateinput:{
-    flex:0.5
-
+  dateinput: {
+    flex: 0.5,
   },
-  backgound:{
+  backgound: {
     //backgroundColor:'yellow',
-    margin:10
-  }
+    margin: 10,
+    marginBottom:20,
+    flex:1,
+  },
 });
 
 export default DaysDate;
